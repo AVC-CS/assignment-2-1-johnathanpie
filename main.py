f17dcff0d59a0f82293bc6decfd01f999b2f11a1
johnathanpie <149451248+johnathanpie@users.noreply.github.com>
2023-11-06 19:59:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    mnum = int(input('Enter the number of male students: '))
+    fnum = int(input('Enter the number of female students: '))
+    total = mnum + fnum
+    m_perc = mnum / total * 100
+    f_perc = fnum / total * 100
 
     """
     ########################################
